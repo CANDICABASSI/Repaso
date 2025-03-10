7096cc02c53e4987f8b68b9c792afd7c1015a364
CANDICABASSI <candecabassi@gmail.com>
2025-03-10 13:01:59 -0300

    Create markdown.py

---
+++ markdown.py
@@ -0,0 +1,11 @@
+Markdown es un lenguaje de marcado ligero que se utiliza para formatear texto de manera sencilla. Fue creado por John Gruber en 2004 con la idea de que se pudiera escribir en un formato fácil de leer y escribir, que luego pudiera ser convertido a HTML. Markdown es ampliamente usado en plataformas como GitHub, foros, blogs, y otros espacios donde se necesita escribir texto con formato de forma rápida y eficiente.
+
+Algunas de las características clave de Markdown incluyen:
+
+Encabezados: Se crean usando el símbolo #, por ejemplo, # Título 1, ## Título 2.
+Listas: Pueden ser ordenadas con números (1. Primer ítem) o desordenadas con guiones (-) o asteriscos (*).
+Enlaces: Se insertan con la sintaxis [texto](URL).
+Imágenes: Similar a los enlaces, pero precedido por un signo de exclamación: ![alt text](URL de la imagen).
+Negritas y cursivas: Se usan asteriscos o guiones bajos, por ejemplo, **negrita** o *cursiva*.
+Citas: Se indican con el símbolo >, por ejemplo, > Cita textual.
+Es muy útil por su simplicidad y la capacidad de convertir el texto formateado en HTML fácilmente.

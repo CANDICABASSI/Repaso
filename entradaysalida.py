06e624cc3ce58e55afd7d0a6470b744af8d04b31
CANDICABASSI <candecabassi@gmail.com>
2025-03-10 12:42:10 -0300

    Create entradaysalida.py

---
+++ entradaysalida.py
@@ -0,0 +1,10 @@
+##actividad: crear un programa que solicite al usuario ingresar una palabra y luego mostrarla por pantalla
+a=input("ingresa una palabra")
+print(a)
+##Crear un programa que le solicite al usuario ingresar un numero entero, si el num es mayor a 10 mostrar por pantalla el numero 10, de lo contrario mostrar por pantalla "fin del programa"
+a=input ("ingresa un num entero ")
+b=10
+if  a>b: 
+print(b)
+else :
+print("fin del programa")
